190cd13da4d3ca6122bdf9ae95a3a27bab595645
Johnellery <123222516+Johnellery@users.noreply.github.com>
2023-02-06 15:29:09 +0800

    Add files via upload

---
+++ EirishPayroll/code/show.php
@@ -0,0 +1,19 @@
+<script>
+function togglePasswordVisibility() {
+  var passwordInput = document.getElementById("password");
+  var cpassInput = document.getElementById("cpassword");
+  
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+  
+  if (cpassInput.type === "cpassword") {
+    cpassInput.type = "text";
+  } else {
+    cpassInput.type = "cpassword";
+  }
+}
+  </script>
+
